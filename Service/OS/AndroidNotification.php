bd31441027b74ba7f0310196152fa40ac72dfbe2
Rich Sage <rich.sage@gmail.com>
2012-05-24 20:55:54 -0500

    Switched to using Buzz.

--- Service/OS/AndroidNotification.php
+++ Service/OS/AndroidNotification.php
@@ -2,6 +2,8 @@
 
 namespace RMS\PushNotificationsBundle\Service\OS;
 
+use Buzz\Browser;
+
 class AndroidNotification implements OSNotificationServiceInterface
 {
     const DEFAULT_COLLAPSE_KEY = 1;
@@ -56,31 +58,23 @@ class AndroidNotification implements OSNotificationServiceInterface
      *
      * @param $deviceToken
      * @param $message
-     * @param $messageType
+     * @param $collapseKey
      * @return bool
      */
-    public function send($deviceToken, $message, $messageType = self::DEFAULT_COLLAPSE_KEY)
+    public function send($deviceToken, $message, $collapseKey = self::DEFAULT_COLLAPSE_KEY)
     {
         if ($this->getAuthToken()) {
             $headers[] = "Authorization: GoogleLogin auth=" . $this->authToken;
             $data = array(
                 "registration_id" => $deviceToken,
-                "collapse_key"    => $messageType,
+                "collapse_key"    => $collapseKey,
                 "data.message"    => $message,
             );
 
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, "https://android.apis.google.com/c2dm/send");
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($curl, CURLOPT_HEADER, 0);
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            $response = curl_exec($curl);
-            curl_close($curl);
-
-            return preg_match("/^id=/", $response) > 0;
+            $buzz = new Browser();
+            $buzz->getClient()->setVerifyPeer(false);
+            $response = $buzz->post("https://android.apis.google.com/c2dm/send", $headers, http_build_query($data));
+            return preg_match("/^id=/", $response->getContent()) > 0;
         }
 
         return false;
@@ -94,14 +88,6 @@ class AndroidNotification implements OSNotificationServiceInterface
      */
     protected function getAuthToken()
     {
-        $curl = curl_init();
-        if (!$curl) {
-            return false;
-        }
-
-        curl_setopt($curl, CURLOPT_URL, "https://www.google.com/accounts/ClientLogin");
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-
         $data = array(
             "Email"         => $this->username,
             "Passwd"        => $this->password,
@@ -110,20 +96,14 @@ class AndroidNotification implements OSNotificationServiceInterface
             "service"       => "ac2dm"
         );
 
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-
-        $response = curl_exec($curl);
-        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
-
-        if ($httpCode !== 200) {
+        $buzz = new Browser();
+        $buzz->getClient()->setVerifyPeer(false);
+        $response = $buzz->post("https://www.google.com/accounts/ClientLogin", array(), http_build_query($data));
+        if ($response->getStatusCode() !== 200) {
             return false;
         }
 
-        preg_match("/Auth=([a-z0-9_\-]+)/i", $response, $matches);
+        preg_match("/Auth=([a-z0-9_\-]+)/i", $response->getContent(), $matches);
         $this->authToken = $matches[1];
         return true;
     }
