5df515112f4db1f830747d3e7b91dabaf077a505
Rich Sage <rich.sage@gmail.com>
2012-06-14 09:49:54 +0100

    Tweaks to Android message.

--- Message/AndroidMessage.php
+++ Message/AndroidMessage.php
@@ -6,12 +6,14 @@ use RMS\PushNotificationsBundle\Device\Types;
 
 class AndroidMessage implements MessageInterface
 {
-	/**
-	 * The data to send in the message
+    const DEFAULT_COLLAPSE_KEY = 1;
+
+    /**
+     * The data to send in the message
      *
      * @var array
-	 */
-	protected $data = array();
+     */
+    protected $data = array();
 
     /**
      * Identifier of the target device
@@ -21,24 +23,31 @@ class AndroidMessage implements MessageInterface
     protected $identifier = "";
 
     /**
+     * Collapse key for data
+     *
+     * @var int
+     */
+    protected $collapseKey = self::DEFAULT_COLLAPSE_KEY;
+
+    /**
      * Sets the data. For Android, this is the entire body of the message
      *
      * @param array $data The data to send
      */
-	public function setData($data)
-	{
-		$this->data = (is_array($data) ? $data : array($data));
-	}
+    public function setData($data)
+    {
+        $this->data = (is_array($data) ? $data : array($data));
+    }
 
     /**
      * Gets the message body to send
      *
      * @return array
      */
-	public function getMessageBody()
-	{
-		return $this->data;
-	}
+    public function getMessageBody()
+    {
+        return $this->data;
+    }
 
     /**
      * Sets the identifier of the target device, eg UUID or similar
@@ -58,5 +67,37 @@ class AndroidMessage implements MessageInterface
     public function getTargetOS()
     {
         return Types::OS_ANDROID;
+    }
+
+    /**
+     * Returns the target device identifier
+     *
+     * @return string
+     */
+    public function getDeviceIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * Android-specific
+     * Returns the collapse key
+     *
+     * @return int
+     */
+    public function getCollapseKey()
+    {
+        return $this->collapseKey;
+    }
+
+    /**
+     * Android-specific
+     * Sets the collapse key
+     *
+     * @param $collapseKey
+     */
+    public function setCollapseKey($collapseKey)
+    {
+        $this->collapseKey = $collapseKey;
     }
 }
--- Message/MessageInterface.php
+++ Message/MessageInterface.php
@@ -4,11 +4,13 @@ namespace RMS\PushNotificationsBundle\Message;
 
 interface MessageInterface
 {
-	public function setData($data);
+    public function setData($data);
 
     public function setDeviceIdentifier($identifier);
 
-	public function getMessageBody();
+    public function getMessageBody();
+
+    public function getDeviceIdentifier();
 
     public function getTargetOS();
 }
