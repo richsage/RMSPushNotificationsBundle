b919a08501addcda810325f564c63f13f4aeb0fd
Robert (Jamie) Munro <rjmunro@arjam.net>
2013-08-15 10:13:04 +0100

    Add bulk notification methods to parent object

--- Service/Notifications.php
+++ Service/Notifications.php
@@ -21,7 +21,31 @@ class Notifications
     }
 
     /**
-     * Sends a message to a device, identified by
+     * Queues a message for sending to a device, identified by
+     * the OS and the supplied device token
+     *
+     * @param \RMS\PushNotificationsBundle\Message\MessageInterface $message
+     * @throws \RuntimeException
+     * @return bool
+     */
+    public function queue(MessageInterface $message)
+    {
+        if (!isset($this->handlers[$message->getTargetOS()])) {
+            throw new \RuntimeException("OS type {$message->getTargetOS()} not supported");
+        }
+
+        $handler = $this->handlers[$message->getTargetOS()];
+
+        if (method_exists($handler, "queue")) {
+            return $handler->queue($message);
+        } else {
+            // Fall back to sending now if bulk messaging not supported
+            return $handler->send($message);
+        }
+    }
+
+    /**
+     * Sends a message to a device instantly, identified by
      * the OS and the supplied device token
      *
      * @param \RMS\PushNotificationsBundle\Message\MessageInterface $message
@@ -38,12 +62,32 @@ class Notifications
     }
 
     /**
+     * Flush - send queued messages in all handlers
+     *
+     * @return array An array of errors returned by each handler, keyed by the OS.
+     */
+    public function flush()
+    {
+        $errors = array();
+        foreach ($this->handlers as $osType => $handler) {
+            if (method_exists($handler, "flush")) {
+                // This is a no-op for platforms that don't support bulk sending
+                $osErrors = $handler->flush();
+                if (count($osErrors)) {
+                    $errors[$osType] = $osErrors;
+                }
+            }
+        }
+        return $errors;
+    }
+
+    /**
      * Adds a handler
      *
      * @param $osType
      * @param $service
      */
-    public function addHandler($osType, $service)
+    public function addHandler($osType, OS\OSNotificationServiceInterface $service)
     {
         if (!isset($this->handlers[$osType])) {
             $this->handlers[$osType] = $service;
